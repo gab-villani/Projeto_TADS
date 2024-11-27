33f9a9bc924720470963183ec3a79b6fafd429cd
gabriela <gabrielavillany70@gmail.com>
2024-11-27 18:11:20 -0400

    a

--- app/app.py
+++ app/app.py
@@ -1,71 +1,20 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from flask_cors import CORS  # Importar a extensão
 import numpy as np
 import os
 
-
-
 app = Flask(__name__)
+CORS(app)  # Habilitar CORS para todas as rotas
 
 # Carregar modelo
-
-# Ler do .env ou variáveis de ambiente
 MODEL_PATH = os.getenv("MODEL_PATH", "/default/path/to/model/flower_model.h5")
-
 print(f"O caminho do modelo é: {MODEL_PATH}")
 model = load_model(MODEL_PATH)
 
 # Classes (modifique de acordo com o dataset)
 CLASS_NAMES = ['Daisy', 'Dandelion', 'Roses', 'Sunflowers', 'Tulips']
-
-# # Caminho para o dataset de validação
-# DATASET_PATH = "flower_photos"  # Altere se necessário
-
-# # Criar geradores globalmente
-# def get_data_generators():
-#     data_gen = ImageDataGenerator(rescale=1.0 / 255, validation_split=0.2)
-    
-#     train_gen = data_gen.flow_from_directory(
-#         DATASET_PATH,
-#         target_size=(150, 150),
-#         batch_size=32,
-#         subset="training",
-#         class_mode="sparse"
-#     )
-
-#     val_gen = data_gen.flow_from_directory(
-#         DATASET_PATH,
-#         target_size=(150, 150),
-#         batch_size=32,
-#         subset="validation",
-#         class_mode="sparse",
-#         shuffle=False  # Importante para manter a ordem dos rótulos
-#     )
-#     return train_gen, val_gen
-
-# train_gen, val_gen = get_data_generators()
-
-# @app.route('/status', methods=['GET'])
-# def status():
-#     # Resetar o gerador de validação
-#     val_gen.reset()
-
-#     # Previsões
-#     y_true = val_gen.classes
-#     y_pred = np.argmax(model.predict(val_gen), axis=-1)
-
-#     # Calcular métricas
-#     accuracy = np.mean(y_true == y_pred)
-#     errors = np.sum(y_true != y_pred)
-#     total = len(y_true)
-
-#     return jsonify({
-#         "accuracy": round(accuracy, 4),
-#         "errors": int(errors),
-#         "total_samples": total
-#     })
 
 @app.route('/classify', methods=['POST'])
 def classify():
@@ -86,7 +35,12 @@ def classify():
     predicted_class = CLASS_NAMES[np.argmax(predictions)]
     confidence = np.max(predictions)
 
-    return jsonify({"class": predicted_class, "confidence": float(confidence)})
+    response = jsonify({"class": predicted_class, "confidence": float(confidence)})
+    
+    # Adicionar cabeçalhos personalizados
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Habilitar qualquer origem
+    response.headers.add("X-Powered-By", "Flask with TensorFlow")
+    return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=500)
